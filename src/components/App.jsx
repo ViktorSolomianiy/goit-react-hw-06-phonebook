9477e0254774fb8fff97af533864d26f131d7cf3
ViktorSolomianiy <xmegasystem35@gmail.com>
2023-03-09 21:16:46 +0200

    completed hw 6

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,26 @@
+import { useSelector } from 'react-redux';
+import { getContacts } from 'redux/selectors';
+import './App.css';
+
+import { ContactsForm } from './Contacts/ContactsForm/ContactForm';
+import { Filter } from './Filter/Filter';
+import { ContactsList } from './Contacts/ContactsList/ContactsList';
+
 export const App = () => {
-  return <div></div>;
+  const contacts = useSelector(getContacts);
+
+  return (
+    <div>
+      <h1 className="title">Phonebook</h1>
+
+      <ContactsForm />
+
+      <div className="contacts__container">
+        <h2 className="contacts__title">Contacts</h2>
+
+        {contacts.length !== 0 && <Filter />}
+        <ContactsList />
+      </div>
+    </div>
+  );
 };
